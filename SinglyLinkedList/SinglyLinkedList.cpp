15847de3c28f7e1a2649a5680c1b3379f9397bdd
katerinavlasova <derkamesser@yandex.ru>
2022-02-07 22:07:50 +0300

    Linked list class and constructor were added

--- SinglyLinkedList/SinglyLinkedList.cpp
+++ SinglyLinkedList/SinglyLinkedList.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+using namespace std;
+
+template <typename TypeValue>
+struct Node {
+    TypeValue value = NULL;
+    Node* nextNode = nullptr;
+    Node<TypeValue>(TypeValue val = TypeValue(), Node* node = nullptr) : value(val), nextNode(node)
+    {
+    };
+};
+
+template <typename TypeValue>
+class LinkedList {
+
+private:
+    Node<TypeValue>* head = nullptr;
+    unsigned int size = 0;
+
+public:
+
+    LinkedList(const TypeValue& value)
+    {
+        Node<TypeValue>* node;
+        node = new Node<TypeValue>(value);
+        if (!node)
+            return;
+        ++size;
+        head = node;
+    }
+};
+
+
 int main()
 {
+    LinkedList<int> listik(5);
     std::cout << "Hello World!\n";
 }
