6bf2a6ae385bea23d0467b644a56e3f66a992daf
katerinavlasova <derkamesser@yandex.ru>
2022-02-08 12:34:13 +0300

    LinkedList class declarations and definitions are separated between header and cpp file

--- SinglyLinkedList/SinglyLinkedList.cpp
+++ SinglyLinkedList/SinglyLinkedList.cpp
@@ -1,207 +1,200 @@
+#include "SinglyLinkedList.h"
 #include <iostream>
 
 using namespace std;
-#include <iterator>
-#include <cstddef>
-
+using namespace List;
 
 template <typename TypeValue>
-class LinkedList {
+LinkedList<TypeValue>::LinkedList(const TypeValue& value)
+{
+    Node<TypeValue>* node;
+    node = new Node<TypeValue>(value);
+    if (!node)
+        return;
+    ++size;
+    head = node;
+}
 
-private:
-    template <typename TypeValue>
-    struct Node {
-        TypeValue value = NULL;
-        Node* nextNode = nullptr;
-        Node<TypeValue>(TypeValue val = TypeValue(), Node* node = nullptr) : value(val), nextNode(node)
-        {
-        };
-    };
-    Node<TypeValue>* head = nullptr;
-    unsigned int size = 0;
-
-public:
-
-    LinkedList(const TypeValue& value)
-    {
-        Node<TypeValue>* node;
-        node = new Node<TypeValue>(value);
-        if (!node)
-            return;
-        ++size;
-        head = node;
-    }
-
-    TypeValue pushBack(const TypeValue& value)
-    {
-        Node<TypeValue>* node = new Node<TypeValue>(value);
-        if (!node)
-            return NULL;
-        ++size;
-        if (head == nullptr)
-        {
-            head = node;
-            return value;
-        }
-        Node<TypeValue>* tmp = head;
-        while (tmp->nextNode != nullptr)
-            tmp = tmp->nextNode;
-        tmp->nextNode = node;
-        return value;
-    }
-
-    TypeValue pushFront(const TypeValue& value)
-    {
-        Node<TypeValue>* node = new Node<TypeValue>(value);
-        if (!node)
-            return NULL;
-        ++size;
-        node->nextNode = head;
-        head = node;
-        return value;
-    }
-
-    TypeValue popFront()
-    {
-        if (head == nullptr)
-            return NULL;
-        --size;
-        Node<TypeValue>* tmp = head->nextNode;
-        TypeValue value = head->value;
-        delete head;
-        head = tmp;
-        return value;
-    }
-
-    TypeValue popBack()
-    {
-        if (head == nullptr)
-            return NULL;
-        --size;
-        Node<TypeValue>* tmp, * last;
-        tmp = head;
-        last = head;
-        while (tmp->nextNode != nullptr)
-        {
-            last = tmp;
-            tmp = tmp->nextNode;
-        }
-        TypeValue value = tmp->value;
-        if (last == tmp)
-        {
-            delete head;
-            head = nullptr;
-            return value;
-        }
-        delete last->nextNode;
-        last->nextNode = nullptr;
-        return value;
-    }
-
-    TypeValue insertAfter(TypeValue value, int index)
-    {
-        int count = 0;
-        ++size;
-        Node<TypeValue> *element = new Node<TypeValue>(value);
-        if (!element)
-            return NULL;
-        if (head == nullptr)
-        {
-            head = element;
-            return value;
-        }
-        Node<TypeValue> *tmp = head;
-        Node<TypeValue> *last = tmp;
-        while (count <= index && tmp != nullptr)
-        {
-            ++count;
-            last = tmp;
-            tmp = tmp->nextNode;
-        }
-        if (tmp != head)
-            element->nextNode = tmp;
-        last->nextNode = element;
-    }
-
-    TypeValue eraseAfter(int index)
-    {
-        int count = 0;
-        TypeValue value;
-        if (head == nullptr)
-            return NULL;
-        Node<TypeValue> *tmp = head;
-        Node<TypeValue> *previous = tmp;
-        while (count <= index && tmp != nullptr)
-        {
-            ++count;
-            previous = tmp;
-            tmp = tmp->nextNode;
-        }
-        
-        if (tmp)
-        {
-            value = tmp->value;
-            previous->nextNode = tmp->nextNode;
-            delete tmp;
-            return value;
-        }
-
+template <typename TypeValue>
+TypeValue LinkedList<TypeValue>::pushBack(const TypeValue& value)
+{
+    Node<TypeValue>* node = new Node<TypeValue>(value);
+    if (!node)
         return NULL;
-    }
-
-    void clear()
+    ++size;
+    if (head == nullptr)
     {
-        if (head == nullptr)
-            return;
-        Node<TypeValue>* tmp = head;
-        Node<TypeValue>* last = head;
-        while (tmp->nextNode != nullptr)
-        {
-            last = tmp;
-            tmp = tmp->nextNode;
-            delete last;
-        }
-        delete tmp;
+        head = node;
+        return value;
+    }
+    Node<TypeValue>* tmp = head;
+    while (tmp->nextNode != nullptr)
+        tmp = tmp->nextNode;
+    tmp->nextNode = node;
+    return value;
+}
+
+template <typename TypeValue>
+TypeValue LinkedList<TypeValue>::pushFront(const TypeValue& value)
+{
+    Node<TypeValue>* node = new Node<TypeValue>(value);
+    if (!node)
+        return NULL;
+    ++size;
+    node->nextNode = head;
+    head = node;
+    return value;
+}
+
+template <typename TypeValue>
+TypeValue LinkedList<TypeValue>::popFront()
+{
+    if (head == nullptr)
+        return NULL;
+    --size;
+    Node<TypeValue>* tmp = head->nextNode;
+    TypeValue value = head->value;
+    delete head;
+    head = tmp;
+    return value;
+}
+
+template <typename TypeValue>
+TypeValue LinkedList<TypeValue>::popBack()
+{
+    if (head == nullptr)
+        return NULL;
+    --size;
+    Node<TypeValue>* tmp, * last;
+    tmp = head;
+    last = head;
+    while (tmp->nextNode != nullptr)
+    {
+        last = tmp;
+        tmp = tmp->nextNode;
+    }
+    TypeValue value = tmp->value;
+    if (last == tmp)
+    {
+        delete head;
         head = nullptr;
-        size = 0;
-        cout << head << endl;
+        return value;
     }
+    delete last->nextNode;
+    last->nextNode = nullptr;
+    return value;
+}
 
-    void printList()
+template <typename TypeValue>
+TypeValue LinkedList<TypeValue>::insertAfter(TypeValue value, int index)
+{
+    int count = 0;
+    ++size;
+    Node<TypeValue> *element = new Node<TypeValue>(value);
+    if (!element)
+        return NULL;
+    if (head == nullptr)
     {
-        if (head == nullptr)
-        {
-            cout << "No elements!" << endl;
-            return;
-        }
-        Node<TypeValue>* tmp = head;
-        while (tmp != nullptr)
-        {
-            cout << tmp->value << " ";
-            tmp = tmp->nextNode;
-        }
-        cout << endl;
+        head = element;
+        return value;
     }
+    Node<TypeValue> *tmp = head;
+    Node<TypeValue> *last = tmp;
+    while (count <= index && tmp != nullptr)
+    {
+        ++count;
+        last = tmp;
+        tmp = tmp->nextNode;
+    }
+    if (tmp != head)
+        element->nextNode = tmp;
+    last->nextNode = element;
+}
 
-    TypeValue front()
-    {
-        if (head == nullptr)
-            return NULL;
-        return head->value;
-    }
+template <typename TypeValue>
+TypeValue LinkedList<TypeValue>::eraseAfter(int index)
+{
+    int count = 0;
+    TypeValue value;
+    if (head == nullptr)
+        return NULL;
+     Node<TypeValue> *tmp = head;
+     Node<TypeValue> *previous = tmp;
+     while (count <= index && tmp != nullptr)
+     {
+         ++count;
+         previous = tmp;
+         tmp = tmp->nextNode;
+     }
+        
+     if (tmp)
+     {
+         value = tmp->value;
+         previous->nextNode = tmp->nextNode;
+         delete tmp;
+         return value;
+     }
+     
+     return NULL;
+}
 
-    bool isEmpty()
+template <typename TypeValue>
+void LinkedList<TypeValue>::clear()
+{
+    if (head == nullptr)
+        return;
+    Node<TypeValue>* tmp = head;
+    Node<TypeValue>* last = head;
+    while (tmp->nextNode != nullptr)
     {
-        if (head == nullptr)
-            return true;
-        return false;
+        last = tmp;
+        tmp = tmp->nextNode;
+        delete last;
     }
+    delete tmp;
+    head = nullptr;
+    size = 0;
+    cout << head << endl;
+}
 
-    unsigned int getSize()
+template <typename TypeValue>
+void LinkedList<TypeValue>::printList()
+{
+    if (head == nullptr)
     {
-        return size;
+        cout << "No elements!" << endl;
+        return;
     }
-};
+    Node<TypeValue>* tmp = head;
+    while (tmp != nullptr)
+    {
+        cout << tmp->value << " ";
+        tmp = tmp->nextNode;
+    }
+    cout << endl;
+}
+
+template <typename TypeValue>
+TypeValue LinkedList<TypeValue>::front()
+{
+    if (head == nullptr)
+        return NULL;
+    return head->value;
+}
+
+template <typename TypeValue>
+bool LinkedList<TypeValue>::isEmpty()
+{
+    if (head == nullptr)
+        return true;
+    return false;
+}
+
+template <typename TypeValue>
+unsigned int LinkedList<TypeValue>::getSize()
+{
+    return size;
+}
 
 
 int main()
---
+++ SinglyLinkedList/SinglyLinkedList.h
@@ -0,0 +1,35 @@
+#include <iostream>
+
+namespace List
+{
+    template <typename TypeValue>
+    class LinkedList {
+    private:
+        template <typename TypeValue>
+        struct Node {
+            TypeValue value = NULL;
+            Node* nextNode = nullptr;
+            Node<TypeValue>(TypeValue val = TypeValue(), Node* node = nullptr) : value(val), nextNode(node)
+            {
+            };
+        };
+
+        Node<TypeValue>* head = nullptr;
+        unsigned int size = 0;
+
+    public:
+        LinkedList(const TypeValue& value);
+
+        TypeValue pushBack(const TypeValue& value);
+        TypeValue pushFront(const TypeValue& value);
+        TypeValue popFront();
+        TypeValue popBack();
+        TypeValue insertAfter(TypeValue value, int index);
+        TypeValue eraseAfter(int index);
+        void clear();
+        void printList();
+        TypeValue front();
+        bool isEmpty();
+        unsigned int getSize();
+        };
+}
